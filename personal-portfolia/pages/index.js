5eab9c097066dc00ada8964628d0b0a4f01d54e4
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2024-01-15 20:06:27 +0800

    update

---
+++ personal-portfolia/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+  <div class="flex justify-spacebetween items-center flex-row w-full h-24 bg-slate-50">
+    <div class="">
+      <h2 class="text-2xl text-slate-950 font-sans">{'<SS />'}</h2>
+    </div>
+  </div>);
+}
